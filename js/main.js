24fb648901c45a3ad66f15060ea45a4039c75752
ThubeesanKugathas <Thubeesan@outlook.com>
2021-12-09 16:09:36 +0100

    js comments

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// obbiettivo: rendere visibile hamburger-menu dopo il click ed invisibile dopo il click del close 
+
+// definire le variabili dei due bottoni 
+const openButton = document.querySelector('.fa-bars');
+const closeButton = document.querySelector('.close');
+
+// definire le variabili delle classi che si modificheranno dopo i click 
+
